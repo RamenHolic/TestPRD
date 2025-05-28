56e3c67513ebe0997c8e5636efec349c62381da2
RamenHolic <yipsrb@gmail.com>
2025-05-29 02:00:41 +0700

    streamlit_chatbot.py

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -12,7 +12,7 @@ user_question = st.text_input("Tanyakan apa pun tentang asuransi:")
 
 # Fungsi untuk kirim ke Hugging Face API
 def ask_huggingface(question):
-    API_URL = "google/flan-t5-large"
+    API_URL = "https://google/flan-t5-large"
     payload = {
         "inputs": question,
         "parameters": {"max_new_tokens": 300}
